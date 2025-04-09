a6a776374ec8e6295a685ac9ecf4702d92ff6391
leikou <koulei@zdongpin.com>
2025-04-09 23:36:00 +0800

    docs: 更新README并添加初始化脚本
    更新了README文件，简化了项目初始化步骤，并添加了`init.sh`脚本以自动化初始化过程。新的README文件更清晰地指导用户如何配置环境变量和执行任务。

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # 项目初始化指南
 
-## 1. 建立虚拟环境
-首先，确保你已经安装了 `poetry`。如果还未安装，可以使用以下命令进行安装：
+## 1. 执行初始化脚本
+执行`init.sh`脚本完成项目初始化，该脚本会安装项目所需的所有依赖。使用以下命令执行脚本：
 ```bash
-pip install poetry
+sh init.sh
 ```
-
-由于 `pyproject.toml` 中设置了 `virtualenvs.in-project = true`，`poetry` 会在项目目录下创建虚拟环境。使用以下命令创建虚拟环境：
-```bash
-poetry install --no-root
-```
-
-## 2. 激活虚拟环境
-使用以下命令激活刚刚创建的虚拟环境：
-```bash
-poetry shell
-```
-
-## 3. 安装依赖
-使用 `poetry` 安装 `poetry.lock` 中的包：
-```bash
-poetry install
-```
-
 这样，项目就完成了初始化，可以开始开发工作了。
+
+## 2. 配置环境变量
+在项目根目录下创建`.env`文件，根据`.env.example`文件的内容填写环境变量。
+
+
+## 3. 如何执行任务
+在项目根目录下执行以下命令：
+```bash
+poetry run command_line_tool.py [task]
+```
+例如，要执行`task1`任务，可以执行以下命令：
+```bash
+poetry run command_line_tool.py task1
+``` 
+查看帮助信息：
+```bash
+poetry run command_line_tool.py -h
+```
---
+++ init.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# 从pyproject.toml中读取Python版本要求
+PYTHON_REQUIRED=$(grep 'python =' pyproject.toml | cut -d'=' -f2 | tr -d ' "\n')
+# 获取当前Python版本
+PYTHON_CURRENT=$(python -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
+# 检查当前Python版本是否符合要求
+echo "开始检查Python版本是否符合要求..."
+if ! python -c "import packaging.version; assert packaging.version.parse('$PYTHON_CURRENT') >= packaging.version.parse('$PYTHON_REQUIRED'.lstrip('^'))" ; then
+    echo "错误：当前Python版本 $PYTHON_CURRENT 不符合 pyproject.toml 中要求的 $PYTHON_REQUIRED 版本。" >&2
+    exit 1
+  else
+    echo "Python版本符合要求，当前Python版本为： $PYTHON_CURRENT ，继续执行。"
+  fi
+
+# 确保已经安装了poetry
+echo "开始检查Poetry是否已经安装..."
+if ! command -v poetry &> /dev/null
+  then
+    echo "Poetry未安装，开始安装..."
+    pip install poetry
+    echo "Poetry安装完成。"
+  else
+    echo "Poetry已安装，无需执行。"
+  fi
+
+# 检查虚拟环境是否已经建立
+echo "开始检查虚拟环境是否已经建立..."
+if [ ! -d ".venv" ]; then
+    echo "虚拟环境未建立，开始建立..."
+    poetry install --no-root
+    echo "虚拟环境建立完成。"
+  else
+    echo "虚拟环境已建立，无需执行。"
+  fi
+
+# 创建datas目录，如果不存在
+echo "开始检查datas目录是否存在..."
+if [ ! -d "datas" ]; then
+    echo "datas目录不存在，开始创建..."
+    mkdir datas
+    echo "datas目录创建完成。"
+  else
+    echo "datas目录已存在，无需执行。"
+  fi
+
+# 检查并安装Playwright浏览器
+echo "开始检查并安装Playwright浏览器..."
+if ! poetry run playwright --version &> /dev/null
+  then
+    echo "Playwright浏览器未安装，开始安装..."
+    poetry run playwright install
+    echo "Playwright浏览器安装完成。"
+  else
+    echo "Playwright浏览器已安装，版本号为：$(poetry run playwright --version)";
+  fi
+  echo "初始化已经完成"
