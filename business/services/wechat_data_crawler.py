91b58bea11efc983e74210e8fb894f11a597b051
leikou <leikou@163.com>
2025-04-15 19:18:28 +0800

    feat: 添加pendulum依赖并优化微信公众号数据爬取逻辑
    添加pendulum库用于日期处理，优化微信公众号数据爬取逻辑，支持指定日期范围下载数据。同时更新.gitignore文件，添加tmp目录忽略规则。

--- business/services/wechat_data_crawler.py
+++ business/services/wechat_data_crawler.py
@@ -25,12 +25,24 @@ from playwright.sync_api import sync_playwright
 import random
 from pathlib import Path
 from config import config
-
+import pendulum
+import re
+import time
 
 class WechatDataFetcher:
-    def __init__(self):
+
+    def __init__(self, p, begin_date=None, end_date=None):
         self.browser = None
         self.page = None
+        self.p = p
+        if begin_date is None:
+            self.begin_date = pendulum.now() - pendulum.duration(days=30)  # 最多2个月的数据
+        else:
+            self.begin_date = begin_date
+        if end_date is None:
+            self.end_date = pendulum.now() - pendulum.duration(days=1)
+        else:
+            self.end_date = end_date
         self.tmp_data_dir = Path(config.root_dir) / 'tmp/data/wechat'
 
     def login(self, remenber=True):
@@ -50,27 +62,26 @@ class WechatDataFetcher:
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
         ]
         
-        with sync_playwright() as p:
-            self.browser = p.chromium.launch(headless=False, downloads_path=self.tmp_data_dir)
-            headers = {'User-Agent': random.choice(user_agents)}
-            self.page = self.browser.new_page(extra_http_headers=headers)
-            
-            if remenber and os.path.exists(session_path):
-                self.page.context.storage_state(path=session_path)
-            
-            self.page.goto('https://mp.weixin.qq.com/')
-            print('请扫码登录微信公众号后台...')
-            self.page.wait_for_url('https://mp.weixin.qq.com/*')
-            if remenber:
-                self.page.context.storage_state(path=session_path)
+        self.browser = self.p.chromium.launch(headless=False, downloads_path=self.tmp_data_dir)
+        headers = {'User-Agent': random.choice(user_agents)}
+        self.page = self.browser.new_page(extra_http_headers=headers)
+        
+        if remenber and os.path.exists(session_path):
+            self.page.context.storage_state(path=session_path)
+        
+        self.page.goto('https://mp.weixin.qq.com/')
+        print('请扫码登录微信公众号后台...')
+        self.page.wait_for_url('https://mp.weixin.qq.com/*')
+        if remenber:
+            self.page.context.storage_state(path=session_path)
 
-            # 等待导航栏加载完成
-            self.page.wait_for_selector('#js_index_menu')
-                
-            # 模拟人类操作行为
-            time.sleep(random.uniform(0.5, 3))
-            scroll_distance = random.randint(100, 500)
-            self.page.evaluate(f'window.scrollBy(0, {scroll_distance});')
+        # 等待导航栏加载完成
+        self.page.wait_for_selector('#js_index_menu')
+            
+        # 模拟人类操作行为
+        time.sleep(random.uniform(0.5, 3))
+        scroll_distance = random.randint(100, 500)
+        self.page.evaluate(f'window.scrollBy(0, {scroll_distance});')
                 
     def get_account_name(self):
         """
@@ -78,9 +89,10 @@ class WechatDataFetcher:
         """
         self.page.wait_for_selector('div.account_box-body > span.acount_box-nickname')
         account_name = self.page.text_content('div.account_box-body > span.acount_box-nickname')
+        print(account_name)
         return account_name.strip()
 
-    def download_account_articles_data(self):
+    def download_traffic_data(self):
         self.page.click('text=数据分析')
         # 随机等待
         time.sleep(random.uniform(0.5, 3))
@@ -94,13 +106,21 @@ class WechatDataFetcher:
         # 随机滚动
         scroll_distance = random.randint(100, 500)
         self.page.evaluate(f'window.scrollBy(0, {scroll_distance});')
-
         # 确保下载链接可见并点击
-        download_link = self.page.wait_for_selector('text=下载数据明细', state='visible')
-        self.page.evaluate('element => element.scrollIntoView()', download_link)
+        # 定位第一个"下载数据明细"超链接，必须等待元素出现
+        self.page.wait_for_selector('text=下载数据明细')
+        download_links = self.page.query_selector_all('text=下载数据明细')
+        first_download_link = download_links[0]
+        # 滑动到第一个元素使其可见
+        self.page.evaluate('element => element.scrollIntoView()', first_download_link)
+        # 等待元素可见
+        self.page.wait_for_selector('text=下载数据明细', state='visible')
         time.sleep(random.uniform(0.5, 2))  # 随机等待确保元素完全可见
-        download_link.click()
-        self._wait_for_download()
+
+        print(self.begin_date.strftime('%Y-%m-%d'), self.end_date.strftime('%Y-%m-%d'))
+        # 根据self.begin_date和self.end_date来选择下载的日期范围
+        _pick_date(self.begin_date, self.end_date, self.page, self.page.query_selector('//form[@class="mass_all_filter"]'))
+        self._wait_for_download(lambda: first_download_link.click(), self.tmp_data_dir / 'traffic_data.xlsx')
 
     def download_article_7d_data(self):
         self.page.click('text=已通知内容')
@@ -115,8 +135,7 @@ class WechatDataFetcher:
             random_element = random.choice(clickable_elements)
             random_element.click(force=True)
             time.sleep(random.uniform(0.5, 3))
-        self.page.click('text=下载数据明细')
-        self._wait_for_download('datas/tmp/article_7d_data.xlsx')
+        self._wait_for_download(lambda: self.page.click('text=下载数据明细'), self.tmp_data_dir / 'article_7d_data.xlsx')
 
     def download_specific_article_data(self):
         self.page.click('text=详情')
@@ -130,13 +149,22 @@ class WechatDataFetcher:
         if clickable_elements:
             random_element = random.choice(clickable_elements)
             random_element.click(force=True)
-            time.sleep(random.uniform(0.5, 3))
-        self.page.click('text=下载数据明细')
-        self._wait_for_download('datas/tmp/specific_article_data.xlsx')
-
-    def _wait_for_download(self, path):
-        while not os.path.exists(path):
-            pass
+            pendulum.sleep(random.uniform(0.5, 3))
+        self._wait_for_download(lambda: self.page.click('text=下载数据明细'), self.tmp_data_dir / 'specific_article_data.xlsx')
+        
+    def _wait_for_download(self, click_action, path=None, timeout=60):
+        with self.page.expect_download() as download_info:
+            click_action()
+        download = download_info.value
+        if path is None:
+            path = self.tmp_data_dir / download.suggested_filename
+        download.save_as(path)
+        # 轮询检查文件是否下载完成
+        start_time = pendulum.now()
+        while not path.exists():
+            if (pendulum.now() - start_time).total_seconds() > timeout:
+                raise TimeoutError('文件下载超时')
+            pendulum.sleep(1)
 
 
 class WechatDataAnalyzer:
@@ -166,9 +194,11 @@ class WechatDataAnalyzer:
 
 
 class WechatDataPipeline:
-    def __init__(self):
-        self.fetcher = WechatDataFetcher()
+
+    def __init__(self, begin_date=None, end_date=None):
         self.analyzer = None
+        self.p = sync_playwright().start()
+        self.fetcher = WechatDataFetcher(self.p, begin_date, end_date)
 
     def login(self):
         self.fetcher.login()
@@ -180,9 +210,9 @@ class WechatDataPipeline:
         return self
 
     def download_all_data(self):
-        self.fetcher.download_account_data()
+        self.fetcher.download_traffic_data()
         self.fetcher.download_article_7d_data()
-        self.fetcher.download_specific_article_data()
+        #self.fetcher.download_specific_article_data()
         self.fetcher.browser.close()
         return self
 
@@ -191,6 +221,105 @@ class WechatDataPipeline:
         return self
 
 
-if __name__ == '__main__':
+def _pick_date(begin: pendulum.DateTime, end: pendulum.DateTime, page, parent):
+    """
+    需要确保日期选择器在页面上显示
+    微信后台日期选择器，选择特定的日期范围。
+    跨度不能超过2个月
+    """
+    # 如果end - begin超过 2各月，throw exception
+    if begin > end:
+        raise ValueError("开始日期不能大于结束日期")
+    if end >= pendulum.today():
+        raise ValueError("结束日期必须早于今日")
+    delta = pendulum.Interval(begin, end)
+    if delta.in_months() >= 2:
+        raise ValueError("日期选择器超出范围，开始结束时间相差不能超过2个月")
+
+    def _abstract_year_month(date_str: str):
+        match = re.search(r"(\d{4})年\s+(\d{1,2})月", date_str)
+        if not match:
+            raise ValueError("日历标题日期格式错误")
+        return int(match.group(1)), int(match.group(2))
+
+    # 打开日期选择器
+    picker_icon = parent.query_selector(
+        '//span[@class="weui-desktop-picker__icon-wrap"]'
+    )
+    picker_icon.click()
+    page.wait_for_timeout(1000)
+
+    pannels = parent.query_selector_all(
+        '//dd[contains(@class, "weui-desktop-picker__dd")]/div[contains(@class, "weui-desktop-picker__panel_day")]'
+    )
+    if len(pannels) < 2:
+        raise ValueError("日历面板数量错误")
+    left_pannel, right_pannel, *_ = pannels
+
+    left_head = left_pannel.query_selector(
+        '//div[contains(@class, "weui-desktop-picker__panel__hd")]'
+    )
+    right_head = right_pannel.query_selector(
+        '//div[contains(@class, "weui-desktop-picker__panel__hd")]'
+    )
+
+    # 选择begin
+    # 判断是否进行日期翻页，计算点击 < or > 按钮的次数，并点击获取指定日期范围的日历
+    # 如果begin小于left pannel head日期,需要向左翻页
+    left_year, left_month = _abstract_year_month(left_head.inner_text())
+    left_min_day = pendulum.date(left_year, left_month, 1)
+    begin_delta = pendulum.Interval(left_min_day, pendulum.date(begin.year, begin.month, 1))
+    left_delta_month = begin_delta.in_months()
+    if left_delta_month < 0:
+        for i in range(abs(left_delta_month)):
+            left_pannel.query_selector("button").click()
+            page.wait_for_timeout(500)
+    # 如果begin大于righ pannel head日期，需要向右翻页
+    if left_delta_month >= 2:
+        for i in range(left_delta_month):
+            right_pannel.query_selector("button").click()
+            page.wait_for_timeout(500)
+
+    # 定位begin对应的日期，点击
+    _, left_month = _abstract_year_month(left_head.inner_text())
+    (
+        _,
+        right_month,
+    ) = _abstract_year_month(right_head.inner_text())
+    pannel = left_pannel if left_month == begin.month else right_pannel
+    pannel.query_selector(
+        f'//tbody//*//a[not(contains(@class, "weui-desktop-picker__faded")) and text()={begin.day}]'
+    ).click()
+
+    # 选择end
+    right_year, right_month = _abstract_year_month(right_head.inner_text())
+    right_min_day = pendulum.date(right_year, right_month, 1)
+    end_delta = pendulum.Interval(right_min_day, pendulum.date(end.year, end.month, 1))
+    right_delta_month = end_delta.in_months()
+    if right_delta_month > 0:
+        for i in range(right_delta_month):
+            right_pannel.query_selector("button").click()
+            page.wait_for_timeout(500)
+    # 定位end对应的日期，点击
+    _, left_month = _abstract_year_month(left_head.inner_text())
+    (
+        _,
+        right_month,
+    ) = _abstract_year_month(right_head.inner_text())
+    pannel = left_pannel if left_month == end.month else right_pannel
+    pannel.query_selector(
+        f'//tbody//*//a[not(contains(@class, "weui-desktop-picker__faded")) and text()={end.day}]'
+    ).click()
+
+    # 关闭picker
+    picker_icon.click()
+
+
+
+def run():
     pipeline = WechatDataPipeline()
     pipeline.login().get_account_name().download_all_data().process_data()
+
+
+if __name__ == '__main__':
+    run()
