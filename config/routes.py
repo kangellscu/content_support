38f7ce9da86127e82bbf68493f887f0fcf1d76bc
leikou <leikou@163.com>
2025-05-30 10:39:41 +0800

    docs(routes): 更新微信数据爬取路由的描述信息
    将wx-data-fetch路由的描述中参数begin_date和end_date标记为可选参数，以更准确地反映接口的实际使用方式

--- config/routes.py
+++ config/routes.py
@@ -1,5 +1,5 @@
 routes = [
     ('wechat-file-2-pdf', 'business.services.wechat_content_crawler.run', '根据Excel文件中的链接爬取微信公众号文章，并保存为PDF文件。'),
-    ('wx-data-fetch', 'business.apps.wechat_data_crawler.run', '调用wechat_data_crawler.py中的run方法，接收参数begin_date和end_date，用于指定要下载的日期范围。'),
+    ('wx-data-fetch', 'business.apps.wechat_data_crawler.run', '调用wechat_data_crawler.py中的run方法，接收可选参数begin_date和end_date，用于指定要下载的日期范围。'),
     ('wx-data-pub', 'business.apps.wechat_data_crawler.publish_wechat_data', '执行微信数据发布功能，接收account_name参数')
 ]
